9c845e7d2b259cdefa442b7db391facf78d625b0
sanjai D <sanjaydharmalingam123@gmail.com>
2026-02-25 11:27:34 +0530

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
 export default defineConfig([
   globalIgnores(['dist']),
   {
@@ -25,5 +24,6 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
+    
   },
 ])
